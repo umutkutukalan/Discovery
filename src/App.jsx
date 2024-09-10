ee56e908f0450ba7af7bf3c4e9c79387a48ea328
Umut Kütükalan <118997325+umutkutukalan@users.noreply.github.com>
2024-09-10 15:10:46 +0300

    10-Sentry-integration-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <div className="main-content">
-        <Hero/>
-        <Highlights/>
-      </div>
-    </Router>
-  );
-}
+import * as Sentry from "@sentry/react";
 
-export default App;
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <div className="main-content">
+          <Hero />
+          <Highlights />
+        </div>
+      </Router>
+    </>
+  );
+};
+
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://aaf7d4fd4b180e37e2016aa8c4bf0357@o4507923958136832.ingest.de.sentry.io/4507928268570704",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.browserProfilingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  profilesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
